b04af6bb029848dba25a7f8e4c45e4dc0f1cd4a9
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-01 15:24:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,8 @@ end
 
 def assign_rooms(array)
   rooms = []
-  array.each_with_index(1) {|x, ind|
-  rooms.push("Hello, #{x}! You'll be assigned to room #{ind}!")}
+  array.each_with_index {|x, ind|
+  rooms.push("Hello, #{x}! You'll be assigned to room #{ind +=1}!")}
   rooms
 end
 
@@ -21,5 +21,5 @@ def printer(attendees)
   attendees.each {|x| printing.push(assign_rooms(attendees))}
   puts printing
 
-
+  
 end
