adfb8c28f86c027353bebdcbe0d185e09a0287ce
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-01 15:32:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ end
 
 def printer(array)
   array.each {|x|
-  puts batch_badge_creator(array)}
+  puts batch_badge_creator}
 #  printing = []
 #  attendees.each {|x| printing.push(batch_badge_creator(attendees))}
 #  attendees.each {|x| printing.push(assign_rooms(attendees))}
